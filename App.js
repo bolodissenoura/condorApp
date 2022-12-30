1bf6b84c42cfdff9ebeaa80c4392da8b7fbc7ce3
bolodissenoura <suamaemedeu@outlook.com>
2022-12-30 15:31:09 -0300

    :tada: Initial Commits

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <Text >hahahhahahahha.js</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
